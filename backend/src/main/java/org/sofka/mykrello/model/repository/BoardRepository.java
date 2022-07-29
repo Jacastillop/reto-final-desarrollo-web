087f85d57bf40cec146d469acbcd520ee96973b8
Jhonatan Castillo <jhonatanbattako@gmail.com>
2022-07-29 15:09:35 -0500

    Se configuro properties para MySQL

--- backend/src/main/java/org/sofka/mykrello/model/repository/BoardRepository.java
+++ backend/src/main/java/org/sofka/mykrello/model/repository/BoardRepository.java
@@ -3,4 +3,7 @@ package org.sofka.mykrello.model.repository;
 import org.sofka.mykrello.model.domain.BoardDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<BoardDomain, Integer> { }
+
+public interface
+BoardRepository extends JpaRepository<BoardDomain, Integer> {
+}
